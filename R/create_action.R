b200ee3d5446b56350c677f8da1b8d63bf0489f7
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-19 19:39:09 +0100

    Improve doc

--- R/create_action.R
+++ R/create_action.R
@@ -1,29 +1,29 @@
-#' Create an action for the player to go left
+#' Create a general action
 #' @param action_type the type of action
 #' @export
 create_action <- function(action_type) {
+  # TODO: check if action_type is a valid action type
   action <- list()
   action$type <- action_type
-  # Perhaps other elements (speed?) here
   action
 }
 
-#' Create an action for the player to go left
+#' Action to make the player go left
 #' @note this function must call \link{create_action} to
 #'   inherit everything what an action is
 #' @export
 create_action_player_left <- function() {
   action <- create_action("player_left")
-  # Perhaps other elements (speed?) here
+  # TODO: if needed, add more parameters for this action
   action
 }
 
-#' Create an action for the player to go right
+#' Action to make the player go right
 #' @note this function must call \link{create_action} to
 #'   inherit everything what an action is
 #' @export
 create_action_player_right <- function() {
   action <- create_action("player_right")
-  # Perhaps other elements (speed?) here
+  # TODO: if needed, add more parameters for this action
   action
 }
