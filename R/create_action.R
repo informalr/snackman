ab2f83bb0486b762017be85b761c7dc13b027237
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-19 19:35:01 +0100

    R ignores images in build

--- R/create_action.R
+++ R/create_action.R
@@ -1,17 +1,29 @@
 #' Create an action for the player to go left
+#' @param action_type the type of action
+#' @export
+create_action <- function(action_type) {
+  action <- list()
+  action$type <- action_type
+  # Perhaps other elements (speed?) here
+  action
+}
+
+#' Create an action for the player to go left
+#' @note this function must call \link{create_action} to
+#'   inherit everything what an action is
 #' @export
 create_action_player_left <- function() {
-  action <- list()
-  action$type <- "player_left"
+  action <- create_action("player_left")
   # Perhaps other elements (speed?) here
   action
 }
 
 #' Create an action for the player to go right
+#' @note this function must call \link{create_action} to
+#'   inherit everything what an action is
 #' @export
 create_action_player_right <- function() {
-  action <- list()
-  action$type <- "player_right"
+  action <- create_action("player_right")
   # Perhaps other elements (speed?) here
   action
 }
