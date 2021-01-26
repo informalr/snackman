6ded3a034e0e9a6081750da02bccd8859f041b7b
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-26 18:38:24 +0100

    progress

--- R/create_game_state.R
+++ R/create_game_state.R
@@ -14,11 +14,13 @@
 #' player$size <- 1
 #' game_state <- create_game_state(arena, player)
 create_game_state <- function(arena, player) {
+  snackman::check_arena(arena)
+  snackman::check_player(player)
   if (!is.character(arena)) stop("The first argument must be a character vector.")
   if (!is.list(player)) stop("The second argument must be a list.")
   game_state <- list()
   game_state$arena <- arena
   gamestate$player <- player
-  check_game_state(game_state)
+  # check_game_state(game_state)
   return(game_state)
 }
--- tests/testthat/test-check_game_state.R
+++ tests/testthat/test-check_game_state.R
@@ -1,17 +1,27 @@
+
 test_that("use", {
-  arena <- c(
-    "XXXXX",
-    "X...X",
-    "X..XX",
-    "X...X",
-    "XXXXX"
-  )
-
-  player <- list()
-  player$x <- 100.0
-  player$y <- 200.0
-  player$size <- 1
-
-  expect_error(check_game_state(list(arena = NULL, player = NULL)) , "Invalid game state!")
-  expect_silent(check_game_state(list(arena = arena, player = player)))
+  # arena <- c(
+  #   "XXXXX",
+  #   "X...X",
+  #   "X..XX",
+  #   "X...X",
+  #   "XXXXX"
+  # )
+  #
+  # player <- list()
+  # player$x <- 100.0
+  # player$y <- 200.0
+  # player$size <- 1
+  #
+  expect_silent(check_game_state(create_test_game_state()))
+  expect_error(
+    check_game_state(
+      list(arena = "nonsense", player = "irrelevant")) ,
+      "Invalid 'arena'"
+    )
+  expect_error(
+    check_game_state(
+      list(arena = create_test_arena(), player = "nonsense")) ,
+      "Invalid 'player'"
+    )
 })
