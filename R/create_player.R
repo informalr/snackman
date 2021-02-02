73038212a404f1de59eac23ae17e78c3e45f84f8
janclod <ctiecher@gmail.com>
2021-02-02 19:16:19 +0100

    Progress

---
+++ R/create_player.R
@@ -0,0 +1,4 @@
+create_player <- function(x = 1, y = 1, size = 1) {
+  player <- list()
+
+}
--- tests/testthat/test-create_game_state.R
+++ tests/testthat/test-create_game_state.R
@@ -6,12 +6,4 @@ test_that("use", {
 })
 
 test_that("misuse", {
-  expect_error(create_game_state(NULL, player),
-               "The first argument must be a character vector.")
-  expect_error(create_game_state(NA, player),
-               "The first argument must be a character vector.")
-  expect_error(create_game_state(arena, NULL),
-               "The second argument must be a list.")
-  expect_error(create_game_state(arena, NA),
-               "The second argument must be a list.")
 })
