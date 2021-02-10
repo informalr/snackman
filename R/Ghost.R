e28930a48864f5a034c42bd6887ffdb5673dfe53
Willy Tadema <friesewoudloper@gmail.com>
2021-02-10 21:16:33 +0100

    Fix develop repo

--- R/Ghost.R
+++ R/Ghost.R
@@ -1,6 +1,6 @@
 library(R6)
 
-Ghost <- R6Class(
+Ghost <- R6Class( # nolint
   "Ghost",
   private = list(
     x = NA
@@ -16,18 +16,15 @@ Ghost <- R6Class(
       if (y < 0) {
         stop("y must be 1 or more")
       }
-      if (!personality %in% c('chase', 'ambush', 'freak', 'idiot')) {
+      if (!personality %in% c("chase", "ambush", "freak", "idiot")) {
         stop("personality must be chase, ambush, freak or idiot")
       }
       private$x <- x
       self$y <- y
       self$personality <- personality
     },
-    print = function(){
+    print = function() {
       print("Ik ben een spookje")
-    },
-
-  )
+    }
   )
 )
-
--- R/create_player.R
+++ R/create_player.R
@@ -3,7 +3,6 @@
 #' @param x x coordinate
 #' @param y y coordinate
 #' @param size size
-#'
 #' @return
 #' @export
 create_player <- function(x = 1, y = 1, size = 1) {
