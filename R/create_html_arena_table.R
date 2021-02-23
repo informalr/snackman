05339a72dde7eb5db863d1abff1189013739a8d0
janclod <ctiecher@gmail.com>
2021-02-23 20:01:40 +0100

    Basic arena UI

--- R/create_html_arena_table.R
+++ R/create_html_arena_table.R
@@ -6,12 +6,20 @@ create_html_arena_table <- function(game_state) {
     shiny::tags$table(
       # Make this into a loop to iterate over arena's rows
       shiny::tags$tr(
-        shiny::tags$td(game_state$arena[1][1], # HIERO
-        shiny::tags$td(game_state$arena[1][2],
-        shiny::tags$td(game_state$arena[1][3]
+        shiny::tags$td(stringr::str_sub(game_state$arena[1], 1, 1)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[1], 2, 2)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[1], 3, 3))
       ),
-      shiny::tags$tr(game_state$arena[2]),
-      shiny::tags$tr(game_state$arena[3])
+      shiny::tags$tr(
+        shiny::tags$td(stringr::str_sub(game_state$arena[2], 1, 1)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[2], 2, 2)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[2], 3, 3))
+      ),
+      shiny::tags$tr(
+        shiny::tags$td(stringr::str_sub(game_state$arena[3], 1, 1)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[3], 2, 2)),
+        shiny::tags$td(stringr::str_sub(game_state$arena[3], 3, 3))
+      )
     )
   )
 }
