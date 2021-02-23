98c6f9cc087c6d42996946dbca096fc52acd9a0d
janclod <ctiecher@gmail.com>
2021-02-23 19:45:21 +0100

    progress <table>

--- R/create_html_arena_table.R
+++ R/create_html_arena_table.R
@@ -2,11 +2,12 @@
 #' @return the table as one line of HTML text
 #' @export
 create_html_arena_table <- function(game_state) {
-  #shiny::tag()
-  html <- paste0(
-    "<table>",
-    paste0("<tr>", game_state$arena, "<tr/>", collapse = ""),
-    "<table/>"
+  shiny::tagList(
+    shiny::tags$table(
+      # Make this into a loop to iterate over arena's rows
+      shiny::tags$tr(game_state$arena[1]),
+      shiny::tags$tr(game_state$arena[2]),
+      shiny::tags$tr(game_state$arena[3])
+    )
   )
-  html
 }
--- tests/testthat/test-create_html_arena_table.R
+++ tests/testthat/test-create_html_arena_table.R
@@ -1,5 +1,6 @@
 test_that("use", {
-  html_text <- create_html_arena_table(create_test_game_state())
+  html_text <- paste0(create_html_arena_table(create_test_game_state()),
+                      collapse = "")
   expect_equal(1, length(html_text))
   expect_equal(1, stringr::str_count(html_text, "<table>"))
   expect_equal(3, stringr::str_count(html_text, "<tr>"))
