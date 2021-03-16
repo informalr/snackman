1047971c35338fc43fcc935375b7918e55e85a06
Willy Tadema <friesewoudloper@gmail.com>
2021-03-16 20:05:24 +0100

    Experiment with Leaflet

--- R/visualize_arena.R
+++ R/visualize_arena.R
@@ -1,3 +1,6 @@
+library(leaflet)
+library(raster)
+
 #' Arena visualiseren
 #'
 #' Een testfunctie om wat dingetjes uit te proberen
@@ -7,4 +10,30 @@
 #'
 #' @examples
 visualize_arena <- function() {
+  # https://leafletjs.com/examples/crs-simple/crs-simple.html
+  # https://stackoverflow.com/questions/47170828/overlay-image-on-r-leaflet-map
+  crs <- leafletCRS(
+    crsClass = "L.CRS.Simple"
+  )
+  file <- paste0("'https://raw.githubusercontent.com/informalr/",
+                 "snackman/master/inst/extdata/sketch_20210112.png';")
+  map <- leaflet(
+    width = 918,
+    height = 548,
+    options = leafletOptions(crs = crs)
+  ) %>%
+    setView(459, 274, 0) %>%
+    htmlwidgets::onRender(
+      paste0("
+        function(el, x) {
+          console.log(this);
+          var myMap = this;
+          var imageUrl = ",
+          file,
+          "var imageBounds = [[0, 0], [548, 918]];
+          L.imageOverlay(imageUrl, imageBounds).addTo(myMap);
+        }
+      ")
+    )
+  map
 }
