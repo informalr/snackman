9a4c0fac34f69c923ec249c0c9a8d53b86bdee63
richelbilderbeek <richel@richelbilderbeek.nl>
2021-03-16 17:54:56 +0100

    Expose #31

---
+++ tests/testthat/test-Action.R
@@ -0,0 +1,16 @@
+test_that("create", {
+  skip("Issue 31. Issue #31")
+  expect_silent(Action$new("player_left"))
+  expect_silent(Action$new("player_right"))
+})
+
+test_that("player_left action", {
+  skip("Issue 31. Issue #31")
+  action_player_left <- Action$new("player_left")
+  expect_equal(action_player_left$type, "player_left")
+})
+
+test_that("player_right action", {
+  action_player_right <- Action$new("player_right")
+  expect_equal(action_player_right$type, "player_right")
+})
