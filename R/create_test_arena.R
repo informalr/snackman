a307fadb26eacc43f74cdd2e8bbac07e972a6dc1
Willy Tadema <friesewoudloper@gmail.com>
2021-03-17 19:35:04 +0100

    Fix lintr issues

--- R/create_test_arena.R
+++ R/create_test_arena.R
@@ -1,11 +1,12 @@
 #' Create a simple test arena
 #' @export
 create_test_arena <- function() {
-  Arena$new(
-    c(
-      "XXX",
-      "X.X",
-      "XXX"
-    )
+  test_arena <- data.frame(
+    V1 = c(0, 0, 0, 0, 0, 0),
+    V2 = c(1, 1, 1, 1, 0, 0),
+    V3 = c(0, 0, 1, 0, 0, 0),
+    V4 = c(1, 1, 1, 1, 1, 1),
+    V5 = c(0, 0, 0, 0, 0, 0)
   )
+  Arena$new(test_arena)
 }
